5a23dcb9479e488434db55e3797f16133fff2bea
elfua360 <michaelortiz05@hotmail.com>
2020-07-22 17:42:02 -0400

    changed config

--- bot.js
+++ bot.js
@@ -1,24 +1,19 @@
 const mongoose = require('mongoose');
 const Discord = require('discord.js');
-const fs = require('fs');
+const {bot_token, wit_token} = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-fs.readFile('bot.json', (err, data) => {
-    if (err) throw err;
-    let config = JSON.parse(data);
-    client.login(config["bot_token"])
-        .then(r => {
-            if (!r)
-                throw err;
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
+client.login(bot_token)
+    .then(r => {
+        if (!r)
+            throw err;
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
 
