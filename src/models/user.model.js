df3e44c93c440f37a28ba5e8a3ce522cbecf684a
elfua360 <michaelortiz05@hotmail.com>
2020-07-22 16:49:24 -0400

    Created a model for users

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    discord_id: {
+        type: String,
+        required: true
+    },
+    privilege: {
+       type: Number,
+       required: true
+    },
+    points: {
+        type: Number,
+        default: 10
+    },
+    aliases: {
+        type: [String],
+        default: undefined
+    }
+});
+
+let user = mongoose.model('user', user, "user");
+module.exports = user;
